dcf7c93a6b0c50ef7beffdb856b3a20fc09da8f5
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-08-04 20:11:11 -0400

    base postgres user entities

--- src/main/kotlin/com/qohash/cabaneio2021/inserter/postgres/entity/PostgresUserEntity.kt
+++ src/main/kotlin/com/qohash/cabaneio2021/inserter/postgres/entity/PostgresUserEntity.kt
@@ -1,12 +1,58 @@
 package com.qohash.cabaneio2021.inserter.postgres.entity
 
+import java.time.Instant
 import java.util.*
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "user")
-data class PostgresUserEntity(
-    @Id val id: UUID
+abstract class PostgresUserEntity(
+    @Id val id: UUID,
+    val handle: String,
+    val name: String,
+    @Column(name = "join_date")
+    val joinDate: Instant,
+)
+
+class PostgresIndividualEntity(
+    id: UUID,
+    handle: String,
+    name: String,
+    joinDate: Instant,
+    @Column(name = "birth_date")
+    val birthDate: Instant,
+    val gender: String
+) : PostgresUserEntity(
+    id = id,
+    handle = handle,
+    name = name,
+    joinDate = joinDate
+)
+
+class PostgresBusinessEntity(
+    id: UUID,
+    handle: String,
+    name: String,
+    joinDate: Instant,
+    @Column(name = "number_of_employees")
+    val numberOfEmployees: Long,
+    val phone: String?,
+    val website: String?,
+    @Column(name = "website_name")
+    val websiteName: String?,
+    @Column(name = "location_address_line")
+    val locationAddressLine: String?,
+    @Column(name = "location_city")
+    val locationCity: String?,
+    @Column(name = "location_country")
+    val locationCountry: String?,
+    val verified: Boolean,
+) : PostgresUserEntity(
+    id = id,
+    handle = handle,
+    name = name,
+    joinDate = joinDate
 )
